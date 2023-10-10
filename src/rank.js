ce1cd6b63f87c05fbb95e82c419a6e55fba9e7b1
fai227 <fai227@outlook.jp>
2023-10-10 17:15:31 +0900

    テスト動くまで

---
+++ src/rank.js
@@ -0,0 +1,3 @@
+function resetRanking(rankingData) {
+
+}
---
+++ src/window.js
@@ -0,0 +1,4 @@
+let wrappingDiv = document.getElementById("wrappingDiv");
+window.addEventListener("resize", () => {
+    wrappingDiv.style.width = "100px";
+});
