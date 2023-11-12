fae503ba8299e73f113a44059fd040691b7b5ca6
fai227 <fai227@outlook.jp>
2023-11-12 17:09:53 +0900

    trim完了

---
+++ _python/make.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import numpy as np
+import json
+
+fileName = "_python/shape.png"
+size = 350
+
+image = np.array(Image.open(fileName))
+
+imageArray = []
+for y in image:
+    row = []
+    for x in y:
+        row.append(x[3] > 128)
+    imageArray.append(row)
+
+vertices = []
+for y in range(len(imageArray)):
+    previous = False
+    for x in range(len(imageArray[y])):
+        if imageArray[y][x] != previous:
+            previous = imageArray[y][x]
+            vertices.append({
+                "x": (x-25) * size / 50,
+                "y": (y-25) * size / 50,
+            })
+
+with open("src/vertices.js", "w") as f:
+    f.write("const eightVertices = " + json.dumps(vertices) + ";")
---
+++ _python/trim.py
@@ -0,0 +1,38 @@
+from PIL import Image
+import numpy as np
+import os
+
+directory_name = "_python/before_trim/"
+
+for file in os.listdir(directory_name):
+    img = Image.open(directory_name + file)
+
+    _, _, _, alpha_list = img.split() 
+    numpy_alpha_list = np.array(alpha_list)
+
+    flag = False
+    upper, lower = 0, 0
+    for y in range(1000):
+        if flag == False:
+            if not np.all(numpy_alpha_list[y] == 0):
+                upper = y
+                flag = True
+        else:
+            if np.all(numpy_alpha_list[y] == 0):
+                lower = y
+                break
+
+    flag = False
+    left, right = 0, 0
+    for x in range(1000):
+        if flag == False:
+            if not np.all(numpy_alpha_list[:, x] == 0):
+                left = x
+                flag = True
+        else:
+            if np.all(numpy_alpha_list[:, x] == 0):
+                right = x
+                break
+
+    img = img.crop((left, upper, right, lower))
+    img.save("_python/after_trim/" + file)
